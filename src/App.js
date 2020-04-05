26539a7086c5bd706c99a020d84fbfe1415ac992
Oleg Yanusik <olegyanusik4@gmail.com>
2020-04-05 17:12:50 +0300

    lesson 12 commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Profile from "./components/Content";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img src='https://www.freelogodesign.org/Content/img/logo-samples/flooop.png'/>
-            </header>
-
-            <nav className='nav'>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className='content'>
-                <div>
-                    <img src='https://cdn.britannica.com/67/19367-050-885866B4/Valley-Taurus-Mountains-Turkey.jpg'/>
-                </div>
-                <div>
-                    Ava +description
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        <div>
-                            post1
-                        </div>
-                        <div>
-                            post2
-                        </div>
-                        <div>
-                            post3 and post 4
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Nav/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.freelogodesign.org/Content/img/logo-samples/flooop.png'/>
+        </header>
+    );
+};
+export default Header;
