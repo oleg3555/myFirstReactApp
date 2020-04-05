7c0d3af0d989e220d6942929d8ed7b0a8eee8c1b
Oleg Yanusik <olegyanusik4@gmail.com>
2020-04-05 15:37:44 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://www.freelogodesign.org/Content/img/logo-samples/flooop.png'/>
+            </header>
+
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://cdn.britannica.com/67/19367-050-885866B4/Valley-Taurus-Mountains-Turkey.jpg'/>
+                </div>
+                <div>
+                    Ava +description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post1
+                        </div>
+                        <div>
+                            post2
+                        </div>
+                        <div>
+                            post3
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
